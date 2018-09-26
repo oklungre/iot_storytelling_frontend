93492799607d3624aaf08edd5c55cae7bcaed750
Lukas Bernhard <L.J.Bernhard@web.de>
2018-09-26 13:31:11 +0200

    basic idea is working, field hitbox is just on the wrong spot

--- java/edu/ntnu/iot_storytelling_sensor/MainActivity.java
+++ java/edu/ntnu/iot_storytelling_sensor/MainActivity.java
@@ -2,12 +2,18 @@ package edu.ntnu.iot_storytelling_sensor;
 
 import android.Manifest;
 import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -21,6 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,7 +53,8 @@ import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnDragListener,
                                                                 TCPInterface,
-                                                                View.OnTouchListener {
+                                                                View.OnTouchListener,
+                                                                SensorEventListener {
     public final static int QR_Call = 0;
     public final static int PERMISSION_REQUEST_CAMERA = 1;
 
@@ -57,6 +65,14 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
     private boolean DATA_SYNCED=false;
     private MediaPlayer m_mediaPlayer;
 
+    public final static double TILT_THRESHOLD = 1.0;
+    public final static float TILT_FACTOR = 1.5f;
+
+    private LinearLayout m_topleft;
+    private LinearLayout m_topright;
+    private LinearLayout m_botleft;
+    private LinearLayout m_botright;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,14 +80,23 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         m_mediaPlayer = new MediaPlayer();
 
+        //declaring Sensor Manager and sensor type
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
         /* Check for permissions */
         check_camera_permission();
 
         /* Drag and Drop Init */
-        findViewById(R.id.field_topleft).setOnDragListener(this);
-        findViewById(R.id.field_topright).setOnDragListener(this);
-        findViewById(R.id.field_bottomleft).setOnDragListener(this);
-        findViewById(R.id.field_bottomright).setOnDragListener(this);
+        m_topleft = findViewById(R.id.field_topleft);
+        m_topleft.setOnDragListener(this);
+        m_topright = findViewById(R.id.field_topright);
+        m_topright.setOnDragListener(this);
+        m_botleft = findViewById(R.id.field_bottomleft);
+        m_botleft.setOnDragListener(this);
+        m_botright = findViewById(R.id.field_bottomright);
+        m_botright.setOnDragListener(this);
         findViewById(R.id.parent_view).setOnDragListener(this);
 
         m_field_obj = (GifImageView) findViewById(R.id.myimage_fields);
@@ -319,5 +344,67 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
             Toast.makeText(getApplicationContext(), "Failed to Download Components",
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    /* TILT SENSOR CALLBACKS */
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        //http://www.devexchanges.info/2015/05/detecting-tilt-device-by-using-sensor.html
+        if(m_rel_obj.getVisibility() != View.VISIBLE) return;
+
+        float x = sensorEvent.values[0] * TILT_FACTOR;
+        if (x < -TILT_THRESHOLD || TILT_THRESHOLD < x) {
+            float pos_x = m_rel_obj.getX();
+            m_rel_obj.setX(pos_x - x);
+        }
+
+        float y = sensorEvent.values[1] * TILT_FACTOR;
+        if (y < -TILT_THRESHOLD || TILT_THRESHOLD < y) {
+            float pos_y = m_rel_obj.getY();
+            m_rel_obj.setY(pos_y - y);
+        }
+        check_collision();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    private void check_collision(){
+        Rect buf_obj = new Rect();
+        m_rel_obj.getHitRect(buf_obj);
+        Rect buf_field = new Rect();
+
+        m_topleft.getHitRect(buf_field);
+        Log.d("COLLISION", "FIELD " + buf_field.toString() + "\nOBJECT " + buf_obj.toString());
+        if(Rect.intersects(buf_obj, buf_field)){
+            set_obj_in_new_field(m_topleft);
+            return;
+        }
+
+        m_topright.getHitRect(buf_field);
+        if(Rect.intersects(buf_obj, buf_field)){
+            set_obj_in_new_field(m_topright);
+            return;
+        }
+
+        m_botleft.getHitRect(buf_field);
+        if(Rect.intersects(buf_obj, buf_field)){
+            set_obj_in_new_field(m_botleft);
+            return;
+        }
+
+        m_botright.getHitRect(buf_field);
+        if(Rect.intersects(buf_obj, buf_field)){
+            set_obj_in_new_field(m_botright);
+        }
+    }
+
+    private void set_obj_in_new_field(LinearLayout new_field){
+        ViewGroup owner = (ViewGroup) m_field_obj.getParent();
+        owner.removeView(m_field_obj);
+        new_field.addView(m_field_obj);
+        m_field_obj.setVisibility(View.VISIBLE);
+        m_rel_obj.setVisibility(View.INVISIBLE);
     }
 }
